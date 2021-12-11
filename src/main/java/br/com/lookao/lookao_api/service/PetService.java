889e81546144cf8129b5259060ed767f158aa7fa
MattP013 <mmateus20pereira@gmail.com>
2021-12-11 01:58:36 -0300

    fix:

--- src/main/java/br/com/lookao/lookao_api/service/PetService.java
+++ src/main/java/br/com/lookao/lookao_api/service/PetService.java
@@ -1,10 +1,11 @@
 package br.com.lookao.lookao_api.service;
 
 import br.com.lookao.lookao_api.entity.Pet;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface PetService {
-    List<Pet> listarPets();
 
+    List<Pet> listarPets();
 }
--- src/main/java/br/com/lookao/lookao_api/service/impl/PetServiceImpl.java
+++ src/main/java/br/com/lookao/lookao_api/service/impl/PetServiceImpl.java
@@ -4,9 +4,11 @@ import br.com.lookao.lookao_api.entity.Pet;
 import br.com.lookao.lookao_api.repository.PetRepository;
 import br.com.lookao.lookao_api.service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PetServiceImpl implements PetService {
 
     @Autowired
