8f7114d93f939a30e67afa537de8793db2103be4
Ariane Leite <arileitte@gmail.com>
2022-03-14 22:31:50 -0300

    fix: renomeando classe de configs do PasswordEncondig

--- src/main/java/br/com/lookao/lookao_api/config/PasswordEncoderConfig.java
+++ src/main/java/br/com/lookao/lookao_api/config/PasswordEncoderConfig.java
@@ -1,12 +1,14 @@
 package br.com.lookao.lookao_api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class EncodeSenhaConfig {
+public class PasswordEncoderConfig {
 
+    @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
