e9ff64f7fe587239bc2564ee5d0705d91cbced94
Ariane Leite <arileitte@gmail.com>
2022-03-14 22:31:07 -0300

    feat: add configs de filtro do spring security

---
+++ src/main/java/br/com/lookao/lookao_api/config/WebSecurityConfig.java
@@ -0,0 +1,73 @@
+package br.com.lookao.lookao_api.config;
+
+import br.com.lookao.lookao_api.entity.Perfil;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String USERS_API_URL = "/user/**";
+    private static final String PETS_ALL_API_URL = "/pet/all";
+    private static final String PETS_OTHERS_API_URL = "/pet/**";
+    private static final String RACA_API_URL = "/raca/**";
+    private static final String SWAGGER_API_URL = "/swagger-ui.html";
+
+    private static final String[] SWAGGER_RESOURCES = {
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/security",
+            "/webjars/**"
+    };
+
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private UserDetailsService userDetailsService;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+       auth.userDetailsService(userDetailsService)
+               .passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests().antMatchers(USERS_API_URL, SWAGGER_API_URL).hasAnyAuthority(Perfil.ADMIN.getDescricao())
+                .antMatchers(PETS_OTHERS_API_URL).hasAnyAuthority(Perfil.ADMIN.getDescricao())
+                .antMatchers(PETS_ALL_API_URL).permitAll()
+                .anyRequest().authenticated()
+                .and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        httpSecurity.headers().frameOptions().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(SWAGGER_RESOURCES);
+    }
+}
